8b1f4b5bee2b3b95053808ccaec4f26fc0339008
goldtool <gladyshev.gladyshev@gmail.com>
2023-02-16 14:43:04 +0300

    Weibull, LongNormal, Pareto

--- src/main/java/MTS/App.java
+++ src/main/java/MTS/App.java
@@ -1,13 +1,11 @@
 package MTS;
 
 
-import MTS.randomGenerators.NormalGenerator;
+import MTS.randomGenerators.ParetoGenerator;
 import MTS.randomGenerators.RandomGenerator;
 import MTS.util.HistBuilder;
 import javafx.application.Application;
 import javafx.stage.Stage;
-
-import java.util.Arrays;
 
 public class App extends Application {
     public static void main(String[] args) {
@@ -16,8 +14,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        /*RandomGenerator normalGenerator = new NormalGenerator(0, 20);
-        double[] normal = normalGenerator.generateArray(-50, 50, 50000);
-        HistBuilder.build(normal, "normal distribution");*/ // образeц
+        RandomGenerator normalGenerator = new ParetoGenerator(2, 7);
+        double[] normal = normalGenerator.generateArray(-50, 50, 50);
+        HistBuilder.build(normal, "Pareto distribution");
     }
 }

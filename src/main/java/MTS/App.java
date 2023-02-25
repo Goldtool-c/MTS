a3170b8e7ccb3defcbc7a2f6a985e6196d8b33ec
goldtool <gladyshev.gladyshev@gmail.com>
2023-02-25 22:55:11 +0300

    resolving conflict app.java

--- src/main/java/MTS/App.java
+++ src/main/java/MTS/App.java
@@ -1,13 +1,11 @@
 package MTS;
 
 
-import MTS.randomGenerators.NormalGenerator;
-import MTS.randomGenerators.RandomGenerator;
-import MTS.util.HistBuilder;
+import MTS.randomGenerators.UniformDistributionGenerator;
+import MTS.util.IndependenceTesting;
+import MTS.util.UniformityTest;
 import javafx.application.Application;
 import javafx.stage.Stage;
-
-import java.util.Arrays;
 
 public class App extends Application {
     public static void main(String[] args) {
@@ -16,27 +14,13 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        RandomGenerator normalGenerator = new ErlangDistributionGenerator(0.5);
-        double[] uniformDistributionGenerator = normalGenerator.generateArray(-50, 50, 500);
-        HistBuilder.build(uniformDistributionGenerator, "Erlang Distribution");
         UniformDistributionGenerator generator = new UniformDistributionGenerator();
         double[] array = generator.generateArray(0,1,100);
-        System.out.println("R = "+IndependenceTesting.coefficientOfCorrelation(array,30));
+        System.out.println("R = "+ IndependenceTesting.coefficientOfCorrelation(array,30) + "| n=100");
+        array = generator.generateArray(0,1,1000);
+        System.out.println("R = "+ IndependenceTesting.coefficientOfCorrelation(array,30) + "| n=1000");
         UniformityTest.test(new UniformDistributionGenerator());
         System.out.println("__________________________________________________");
-        /*UniformityTest.test(new NormalGenerator(1, 1));
-        System.out.println("__________________________________________________");
-        UniformityTest.test(new PoissonDistributionGenerator(1));
-        System.out.println("__________________________________________________");
-        UniformityTest.test(new ParetoGenerator(1, 0));
-        System.out.println("__________________________________________________");
-        UniformityTest.test(new LogNormalGenerator(1, 1));
-        System.out.println("__________________________________________________");
-        UniformityTest.test(new ErlangDistributionGenerator(1));
-        System.out.println("__________________________________________________");
-        UniformityTest.test(new ExponentialDistributionGenerator(1));
-        System.out.println("__________________________________________________");
-        UniformityTest.test(new WeibullGenerator(1, 1, 0));
-        System.out.println("__________________________________________________");*/
+
     }
 }

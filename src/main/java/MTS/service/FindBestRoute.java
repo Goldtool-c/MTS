17969e4d07abe66925db3b5eb9de24d6e92609f7
goldtool <gladyshev.gladyshev@gmail.com>
2023-03-17 21:41:27 +0300

    marks

---
+++ src/main/java/MTS/service/FindBestRoute.java
@@ -0,0 +1,23 @@
+package MTS.service;
+
+import MTS.App;
+import MTS.entity.Flow;
+import MTS.util.EIGRPMark;
+
+public class FindBestRoute {
+    public static Flow find(){
+        Flow res = App.flows[0];
+        double mark = 0;
+        System.out.println("==============================================");
+        for (int i = 0; i < App.flows.length; i++) {
+            if(EIGRPMark.calculateMark(App.flows[i])>mark){
+                res = App.flows[i];
+                mark = EIGRPMark.calculateMark(App.flows[i]);
+            }
+            System.out.println("Flow "+(i+1)+ " mark = "+EIGRPMark.calculateMark(App.flows[i]));
+        }
+        System.out.println(res.toString() + " " + mark);
+        System.out.println("==============================================");
+        return res;
+    }
+}
